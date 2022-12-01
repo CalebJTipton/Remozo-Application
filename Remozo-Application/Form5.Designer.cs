47b2f446215d9e4820bc111d8c65c3c5aea29420
1michaelullrich <michael.ullrich@und.edu>
2022-12-01 16:12:08 -0600

    Resized to 523,1141

--- Remozo-Application/Form5.Designer.cs
+++ Remozo-Application/Form5.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(locate));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btn_disable = new System.Windows.Forms.Button();
             this.btn_displayLog = new System.Windows.Forms.Button();
@@ -37,36 +38,36 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::Remozo_Application.Properties.Resources.App___Locate;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(527, 978);
+            this.pictureBox1.Size = new System.Drawing.Size(523, 1141);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // btn_disable
             // 
-            this.btn_disable.Location = new System.Drawing.Point(122, 857);
+            this.btn_disable.Location = new System.Drawing.Point(100, 966);
             this.btn_disable.Name = "btn_disable";
-            this.btn_disable.Size = new System.Drawing.Size(294, 60);
+            this.btn_disable.Size = new System.Drawing.Size(327, 60);
             this.btn_disable.TabIndex = 1;
             this.btn_disable.Text = "Disable Vehicle";
             this.btn_disable.UseVisualStyleBackColor = true;
             // 
             // btn_displayLog
             // 
-            this.btn_displayLog.Location = new System.Drawing.Point(191, 809);
+            this.btn_displayLog.Location = new System.Drawing.Point(174, 915);
             this.btn_displayLog.Name = "btn_displayLog";
-            this.btn_displayLog.Size = new System.Drawing.Size(169, 30);
+            this.btn_displayLog.Size = new System.Drawing.Size(190, 30);
             this.btn_displayLog.TabIndex = 2;
             this.btn_displayLog.Text = "View Activity Log";
             this.btn_displayLog.UseVisualStyleBackColor = true;
             // 
             // btn_back
             // 
-            this.btn_back.Location = new System.Drawing.Point(41, 134);
+            this.btn_back.Location = new System.Drawing.Point(0, 154);
             this.btn_back.Name = "btn_back";
-            this.btn_back.Size = new System.Drawing.Size(98, 37);
+            this.btn_back.Size = new System.Drawing.Size(110, 37);
             this.btn_back.TabIndex = 3;
             this.btn_back.Text = "Back";
             this.btn_back.UseVisualStyleBackColor = true;
@@ -75,13 +76,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(524, 974);
+            this.ClientSize = new System.Drawing.Size(507, 1102);
             this.Controls.Add(this.btn_back);
             this.Controls.Add(this.btn_displayLog);
             this.Controls.Add(this.btn_disable);
             this.Controls.Add(this.pictureBox1);
-            this.MaximumSize = new System.Drawing.Size(540, 1013);
-            this.MinimumSize = new System.Drawing.Size(540, 1013);
+            this.MaximumSize = new System.Drawing.Size(523, 1141);
+            this.MinimumSize = new System.Drawing.Size(523, 1141);
             this.Name = "locate";
             this.Text = "Locate";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
