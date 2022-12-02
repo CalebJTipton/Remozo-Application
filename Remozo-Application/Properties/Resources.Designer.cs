60509a82744614834ca916448fc60e5f739b24fc
1michaelullrich <michael.ullrich@und.edu>
2022-12-02 13:38:33 -0600

    updated Locate

--- Remozo-Application/Properties/Resources.Designer.cs
+++ Remozo-Application/Properties/Resources.Designer.cs
@@ -189,5 +189,15 @@ namespace Remozo_Application.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Locate {
+            get {
+                object obj = ResourceManager.GetObject("Locate", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
