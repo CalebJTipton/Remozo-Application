884357f92f11edc41636044b160b54eb160e7ca1
1michaelullrich <michael.ullrich@und.edu>
2022-12-02 13:41:58 -0600

    updated settings

--- Remozo-Application/settings.Designer.cs
+++ Remozo-Application/settings.Designer.cs
@@ -28,21 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txt_geoFence = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.btn_checkUpdates = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Remozo_Application.Properties.Resources.Settings;
+            this.pictureBox1.Location = new System.Drawing.Point(2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(526, 1109);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txt_geoFence
+            // 
+            this.txt_geoFence.Location = new System.Drawing.Point(33, 570);
+            this.txt_geoFence.Name = "txt_geoFence";
+            this.txt_geoFence.Size = new System.Drawing.Size(396, 23);
+            this.txt_geoFence.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 951);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(399, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btn_back
+            // 
+            this.btn_back.BackColor = System.Drawing.Color.White;
+            this.btn_back.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_back.Font = new System.Drawing.Font("Segoe UI", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.btn_back.Location = new System.Drawing.Point(45, 154);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(75, 30);
+            this.btn_back.TabIndex = 7;
+            this.btn_back.Text = "Back";
+            this.btn_back.UseVisualStyleBackColor = false;
+            // 
+            // btn_checkUpdates
+            // 
+            this.btn_checkUpdates.BackColor = System.Drawing.Color.Red;
+            this.btn_checkUpdates.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_checkUpdates.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_checkUpdates.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_checkUpdates.ForeColor = System.Drawing.Color.White;
+            this.btn_checkUpdates.Location = new System.Drawing.Point(67, 1057);
+            this.btn_checkUpdates.Name = "btn_checkUpdates";
+            this.btn_checkUpdates.Size = new System.Drawing.Size(402, 39);
+            this.btn_checkUpdates.TabIndex = 8;
+            this.btn_checkUpdates.Text = "Check for Updates";
+            this.btn_checkUpdates.UseVisualStyleBackColor = false;
             // 
             // settings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(522, 1102);
+            this.Controls.Add(this.btn_checkUpdates);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txt_geoFence);
+            this.Controls.Add(this.pictureBox1);
             this.MaximumSize = new System.Drawing.Size(538, 1141);
             this.MinimumSize = new System.Drawing.Size(538, 1141);
             this.Name = "settings";
             this.Text = "settings";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private TextBox txt_geoFence;
+        private TextBox textBox1;
+        private Button btn_back;
+        private Button btn_checkUpdates;
     }
 }
