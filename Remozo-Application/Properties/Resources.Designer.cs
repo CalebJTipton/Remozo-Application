91c8d2b3712a2b910a6f6f1b6008c67c78a6e27b
1michaelullrich <michael.ullrich@und.edu>
2022-12-02 13:30:49 -0600

    Updated control page

--- Remozo-Application/Properties/Resources.Designer.cs
+++ Remozo-Application/Properties/Resources.Designer.cs
@@ -179,5 +179,15 @@ namespace Remozo_Application.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Control {
+            get {
+                object obj = ResourceManager.GetObject("Control", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
--- Remozo-Application/control.Designer.cs
+++ Remozo-Application/control.Designer.cs
@@ -28,21 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.btn_diableVehicle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Remozo_Application.Properties.Resources.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(526, 1108);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_back
+            // 
+            this.btn_back.BackColor = System.Drawing.Color.White;
+            this.btn_back.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_back.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_back.Font = new System.Drawing.Font("Segoe UI", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.btn_back.Location = new System.Drawing.Point(38, 155);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(75, 30);
+            this.btn_back.TabIndex = 3;
+            this.btn_back.Text = "Back";
+            this.btn_back.UseVisualStyleBackColor = false;
+            // 
+            // btn_diableVehicle
+            // 
+            this.btn_diableVehicle.BackColor = System.Drawing.Color.Red;
+            this.btn_diableVehicle.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btn_diableVehicle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_diableVehicle.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_diableVehicle.ForeColor = System.Drawing.Color.White;
+            this.btn_diableVehicle.Location = new System.Drawing.Point(85, 412);
+            this.btn_diableVehicle.Name = "btn_diableVehicle";
+            this.btn_diableVehicle.Size = new System.Drawing.Size(347, 73);
+            this.btn_diableVehicle.TabIndex = 4;
+            this.btn_diableVehicle.Text = "Disable Vehicle";
+            this.btn_diableVehicle.UseVisualStyleBackColor = false;
             // 
             // control
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(522, 1102);
+            this.Controls.Add(this.btn_diableVehicle);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.pictureBox1);
             this.MaximumSize = new System.Drawing.Size(538, 1141);
             this.MinimumSize = new System.Drawing.Size(538, 1141);
             this.Name = "control";
             this.Text = "control";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button btn_back;
+        private Button btn_diableVehicle;
     }
 }
